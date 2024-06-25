f5a0b80e89c6e1c5f4c92806c29bd72ae42b0e06
atabekkr <atabekdev@gmail.com>
2024-06-25 19:26:21 +0500

    solving problems

--- lib/home/bloc/home_state.dart
+++ lib/home/bloc/home_state.dart
@@ -2,6 +2,7 @@ part of 'home_bloc.dart';
 
 abstract class HomeState extends Equatable {
   const HomeState();
+
   @override
   List<Object> get props => [];
 }
@@ -11,8 +12,11 @@ class HomeInitial extends HomeState {}
 class ProductLoadingState extends HomeState {}
 
 class ProductLoadedState extends HomeState {
+
   const ProductLoadedState({required this.data});
+
   final Data data;
+
   @override
   List<Object> get props => [data];
 }
@@ -20,6 +24,8 @@ class ProductLoadedState extends HomeState {
 class ProductEmptyState extends HomeState {}
 
 class ProductLoadingFailedState extends HomeState {
+
   const ProductLoadingFailedState({required this.errorMessage});
+
   final String errorMessage;
 }
